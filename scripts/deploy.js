63770dc9a0251543a73134b0eb326c895602c80b
mzawadi <mwaranguallan345@gmail.com>
2022-06-23 21:07:01 +0300

    add ERC20/IERC20 contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const bKES = await hre.ethers.getContractFactory("Greeter");
+  const bkes = await bKES.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await bkes.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("bKES deployed to:", bkes.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
