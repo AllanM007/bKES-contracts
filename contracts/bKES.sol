903ce380474166e66919ef4f0e6ff3e4e3ec740c
mzawadi <mwaranguallan345@gmail.com>
2022-06-25 11:45:51 +0300

    ERC20 contracts

---
+++ contracts/bKES.sol
@@ -0,0 +1,37 @@
+/// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+import {ERC20Permit} from "./ERC20.sol";
+
+contract bKES is ERC20Permit{
+
+    address private owner;
+    uint256 totalSupply;
+    ERC20Permit token;
+
+    event Mint(address account, uint256 amount);
+    event Burn(address account, uint256 amount);
+
+    constructor() ERC20Permit("BitKES", "bKES"){}
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Unauthorized address");
+        _;
+    }
+
+    function mintbKES(address account, uint256 _amount) external onlyOwner {
+        mint(account, _amount);
+
+        totalSupply += _amount;
+
+        emit Mint(account, _amount);
+    }
+
+    function burnbKES(address account, uint256 _amount) external onlyOwner{
+        burn(account, _amount);
+
+        totalSupply -= _amount;
+
+        emit Burn(account, _amount);
+    }
+}
