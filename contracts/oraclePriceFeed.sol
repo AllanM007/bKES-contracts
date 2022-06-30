3209625790fe2c6b75fbb27a212a5a4a4fc110a2
mzawadi <mwaranguallan345@gmail.com>
2022-06-30 18:16:22 +0300

    update oraclePriceFeed.sol

--- contracts/oraclePriceFeed.sol
+++ contracts/oraclePriceFeed.sol
@@ -1,88 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
-import '@chainlink/contracts/src/v0.8/ChainlinkClient.sol';
-import '@chainlink/contracts/src/v0.8/ConfirmedOwner.sol';
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
-/**
- * Request testnet LINK and ETH here: https://faucets.chain.link/
- * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
- */
-
-/**
- * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
- * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
- */
-contract APIConsumer is ChainlinkClient, ConfirmedOwner {
+contract APIConsumer is ChainlinkClient {
     using Chainlink for Chainlink.Request;
 
     uint256 public price;
+
+    address private oracle;
     bytes32 private jobId;
     uint256 private fee;
 
-    event RequestPrice(bytes32 indexed requestId, uint256 price);
-
     /**
-     * @notice Initialize the link token and target oracle
-     *
-     * Mumbai Testnet details:
-     * Link Token: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
-     * Oracle: 0xf3FBB7f3391F62C8fe53f89B41dFC8159EE9653f (Chainlink DevRel)
-     * jobId: ca98366cc7314957b8c012c72f05aeeb
-     *
+     * Network: Polygon Mumbai Testnet
+     * Oracle: 0x58bbdbfb6fca3129b91f0dbe372098123b38b5e9
+     * Job ID: da20aae0e4c843f6949e5cb3f7cfe8c4
+     * LINK address: 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+     * Fee: 0.01 LINK
      */
-    constructor() ConfirmedOwner(msg.sender) {
+    constructor() {
         setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
-        setChainlinkOracle(0xf3FBB7f3391F62C8fe53f89B41dFC8159EE9653f);
-        jobId = 'ca98366cc7314957b8c012c72f05aeeb';
-        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
+        oracle = 0x58BBDbfb6fca3129b91f0DBE372098123B38B5e9;
+        jobId = "da20aae0e4c843f6949e5cb3f7cfe8c4";
+        fee = 10**16; // 0.01 LINK
     }
 
     /**
-     * Create a Chainlink request to retrieve API response, find the target
-     * data, then multiply by 1000000000000000000 (to remove decimal places from data).
+     * Create a Chainlink request to retrieve API response, find the target price
+     * data, then multiply by 100 (to remove decimal places from price).
      */
-    function requestPriceData() public returns (bytes32 requestId) {
-        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+    function requestMATICKESPrice() public returns (bytes32 requestId) {
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfill.selector
+        );
 
         // Set the URL to perform the GET request on
-        req.add('get', 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=KES');
+        // NOTE: If this oracle gets more than 5 requests from this job at a time, it will not return.
+        req.add(
+            "get",
+            "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=MATIC&to_currency=KES&apikey=E1MLPFUNW7DZE8KP"
+        );
 
         // Set the path to find the desired data in the API response, where the response format is:
-        // {"RAW":
-        //   {"ETH":
-        //    {"KES":
-        //     {
-        //      "PRICE": xxx.xxx,
+        // {
+        //     "Realtime Currency Exchange Rate": {
+        //       "1. From_Currency Code": "BTC",
+        //       "2. From_Currency Name": "Bitcoin",
+        //       "3. To_Currency Code": "CNY",
+        //       "4. To_Currency Name": "Chinese Yuan",
+        //       "5. Exchange Rate": "207838.88814500",
+        //       "6. Last Refreshed": "2021-01-26 11:11:07",
+        //       "7. Time Zone": "UTC",
+        //      "8. Bid Price": "207838.82343000",
+        //       "9. Ask Price": "207838.88814500"
         //     }
-        //    }
-        //   }
-        //  }
-        // request.add("path", "RAW.ETH.KES.PRICE"); // Chainlink nodes prior to 1.0.0 support this format
-        req.add('path', 'RAW,ETH,KES,PRICE'); // Chainlink nodes 1.0.0 and later support this format
+        //     }
+        string[] memory path = new string[](2);
+        path[0] = "Realtime Currency Exchange Rate";
+        path[1] = "5. Exchange Rate";
+        req.addStringArray("path", path);
 
-        // Multiply the result by 1000000000000000000 to remove decimals
-        int256 timesAmount = 10**18;
-        req.addInt('times', timesAmount);
+        // Multiply the result by 10000000000 to remove decimals
+        req.addInt("times", 10000000000);
 
         // Sends the request
-        return sendChainlinkRequest(req, fee);
+        return sendChainlinkRequestTo(oracle, req, fee);
     }
 
     /**
      * Receive the response in the form of uint256
      */
-    function fulfill(bytes32 _requestId, uint256 _price) public recordChainlinkFulfillment(_requestId) {
-        emit RequestPrice(_requestId, _price);
+    function fulfill(bytes32 _requestId, uint256 _price)
+        public
+        recordChainlinkFulfillment(_requestId)
+    {
         price = _price;
     }
-
-    /**
-     * Allow withdraw of Link tokens from the contract
-     */
-    function withdrawLink() public onlyOwner {
-        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-        require(link.transfer(msg.sender, link.balanceOf(address(this))), 'Unable to transfer');
-    }
 }
-
--- contracts/priceFeed.sol
+++ contracts/priceFeed.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+///SPDX-License-Identifier :MIT
+pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
@@ -8,29 +8,25 @@ contract PriceConsumerV3 {
     AggregatorV3Interface internal priceFeed;
 
     /**
-     * Network: Mumbai
+     * Network: Mumbai Testnet 
      * Aggregator: MATIC/USD
      * Address: 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada
      */
-    constructor() {
+    constructor() public {
         priceFeed = AggregatorV3Interface(0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada);
     }
 
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int) {
+    function getLatestPrice() public view returns (uint80, int, uint, uint, uint80) {
         (
-            /*uint80 roundID*/,
-            int256 price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
         ) = priceFeed.latestRoundData();
-        return price;
-    }
-
-    function getDecimalPlaces() public view returns(uint8){
-        return priceFeed.decimals();
+        return (roundID, price, startedAt, timeStamp, answeredInRound);
     }
 }
