348b2ad7135e791170059ac6190adcd6098828ac
mzawadi <mwaranguallan345@gmail.com>
2022-08-14 15:11:16 +0300

    update burn functionality

--- public/server.js
+++ public/server.js
@@ -14,9 +14,11 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const collateralAdapter = process.env.collateralAdapter_ADDRESS;
 const oracleAddress = process.env.oracleContract_ADDRESS;
 const openseaOracleAddress = process.env.openseaOracle_ADDRESS;
+const bKESTokenAddress = process.env.bKES_ADDRESS;
 
 // const { json } = require("hardhat/internal/core/par
 const { ethers } = require("ethers");
+const tokenABI = require("../artifacts/contracts/ERC20.sol/ERC20.json");
 const openseaOracleContractABI = require("../artifacts/contracts/oracleOpenSea.sol/OpenSeaAPIConsumer.json");
 const oracleContractABI = require("../artifacts/contracts/oraclePriceFeed.sol/APIConsumer.json");
 const mintContract = require("../artifacts/contracts/bKESDispatcher.sol/bKES.json");
@@ -52,6 +54,13 @@ const OpenseaOracleContract = new ethers.Contract(
 const collateralAdapterContract = new ethers.Contract(
   collateralAdapter,
   collateralAdapterABI.abi,
+  signer
+);
+
+// Token Contract
+const bKESTokenContract = new ethers.Contract(
+  bKESTokenAddress,
+  tokenABI.abi,
   signer
 );
 
@@ -139,6 +148,20 @@ router.get('/getVaultBalance', async function(req,res){
   res.json(context);
 });
 
+router.get('/getAccountbKESBalance', async function(req,res){
+
+  const bkesBalance = await bKESTokenContract.balanceOf("0x15cdCBB08cd5b2543A8E009Dbf5a6C6d7D2aB53d");
+
+  const fmtBKESBalance = bkesBalance.toString();
+  
+  var context = {
+    bKESAmount: fmtBKESBalance
+  };
+  console.log("Vault Balance:", fmtBKESBalance);
+
+  res.json(context);
+});
+
 router.get('/getActiveDebtBalance', async function(req,res){
 
   const activeDebt = await collateralAdapterContract.ActiveDebtAmount("0x15cdCBB08cd5b2543A8E009Dbf5a6C6d7D2aB53d");
@@ -193,8 +216,13 @@ async function mintbKES(usrAddress, mintAmount) {
     const [to, value] = mintObject.args;
 
     console.log(to, value.toString());
+
+    return "Mint Succesful";
+
   } catch (error) {
     console.log(error);
+
+    return(error);
   }
 }
 
@@ -202,7 +230,57 @@ router.get('/burn', function(req,res){
   res.sendFile(path.join(__dirname+'/burn.html'));
 });
 
+router.post('/burnbKES', function(req, res){
+  var data = req.body;
+  console.log(data);
 
+  var usrAddress = data.address;
+  var amount = data.amount;
+  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(burnbKES(usrAddress, amount));
+    }, 2000);
+  });
+});
+
+async function burnbKES(usrAddress, burnAmount) {
+  const gasPrice = await alchemyProvider.getGasPrice();
+
+  const formattedGasPrice = gasPrice.toString();
+
+  console.log(formattedGasPrice);
+
+  try {
+
+    const burnbKEStx = await collateralAdapterContract.connect(signer).initiateBurn(
+      usrAddress,
+      burnAmount,
+      { gasLimit: 1000000 }
+    );
+
+    console.log(burnbKEStx);
+
+    const burnbKESObject = await burnbKEStx.wait();
+
+    console.log(burnbKESObject);
+
+    const burnObject = burnbKESObject.events.find(
+      (event) => event.event === "successfulbKESBurn"
+    );
+
+    const [to, value] = burnObject.args;
+
+    console.log(to, value.toString());
+
+    return "Burn Succesful";
+
+  } catch (error) {
+    console.log(error);
+
+    return(error);
+  }
+}
 
 router.get('/debtPosition', function(req,res){
   res.sendFile(path.join(__dirname+'/debtPositions.html'));
